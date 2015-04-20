0aa85b927ccf37f3df51320ab3ef1274af71fd36
Alexander Reitzel <funtimecoding@gmail.com>
2015-04-20 12:38:43 +0200

    improve scan-projects script

--- bin/scan-projects.sh
+++ bin/scan-projects.sh
@@ -12,12 +12,34 @@ for PROJECT in ${PROJECTS}; do
     for JOB in ${JOBS}; do
         if [ "${PROJECT}" = "${JOB}" ]; then
             JOB_FOUND=1
+            break
         fi
     done
 
+    DIR="${PROJECT_DIR}/${PROJECT}"
+    cd "${DIR}"
+
+    echo "${PROJECT}"
+
     if [ "${JOB_FOUND}" = "0" ]; then
-        echo "Without CI job: ${PROJECT}"
+        echo "- does not have a CI job"
+    fi
+
+    if [ -d ".git" ]; then
+        REMOTES=$(git remote -v)
+
+        if [ "${REMOTES}" = ""  ]; then
+            echo "- has no git remote"
+        else
+            URL=$(echo ${REMOTES} | awk '{ print $2 }')
+            REPO="${URL##*/}"
+            REPO="${REPO%.git}"
+
+            if [ ! "${REPO}" = "${PROJECT}" ]; then
+                echo "- name differs locally and in CI"
+            fi
+        fi
     else
-        echo "With CI job: ${PROJECT}"
+        echo "- is not a git repository"
     fi
 done
