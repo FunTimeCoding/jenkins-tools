87d283bc9dd51e23b2e458e76fab034e442a0d93
Alexander Reitzel <funtimecoding@gmail.com>
2015-06-06 15:51:36 +0200

    add some more plugins, make reset-security script work on linux and osx

--- bin/install-plugins.sh
+++ bin/install-plugins.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(cd "$(dirname "${0}")"; pwd)
 . "${SCRIPT_DIR}/../lib/jenkins.sh"
 jenkins_auth
-#${JENKINS_CMD} install-plugin git disk-usage warnings log-parser gravatar gitlab-hook cobertura rubyMetrics robot php nodejs
-${JENKINS_CMD} install-plugin git gravatar robot php
+# warnings log-parser cobertura rubyMetrics nodejs
+${JENKINS_CMD} install-plugin git gravatar robot php embeddable-build-status gitlab-hook disk-usage greenballs
 echo "Restarting."
 ${JENKINS_CMD} safe-restart
--- bin/reset-security.sh
+++ bin/reset-security.sh
@@ -1,16 +1,34 @@
 #!/bin/sh -e
 # This script basically does this: https://wiki.jenkins-ci.org/display/JENKINS/Disable+security
 
-ENABLED=$(xml sel --template --value-of "/hudson/useSecurity" ~/.jenkins/config.xml)
+OS=$(uname)
+
+if [ "${OS}" = "Linux" ]; then
+    JENKINS_CONFIG="/var/lib/jenkins/config.xml"
+elif [ "${OS}" = "Darwin" ]; then
+    JENKINS_CONFIG="${HOME}/.jenkins/config.xml"
+fi
+
+ENABLED=$(xml sel --template --value-of "/hudson/useSecurity" "${JENKINS_CONFIG}")
 
 if [ "${ENABLED}" = "false" ]; then
     echo "Security is already disabled."
     exit 0
 fi
 
-launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.jenkins-lts.plist
+if [ "${OS}" = "Linux" ]; then
+    sudo service jenkins stop
+elif [ "${OS}" = "Darwin" ]; then
+    launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.jenkins-lts.plist
+fi
+
 xml edit --inplace --update "/hudson/useSecurity" --value "false" ~/.jenkins/config.xml
 xml edit --inplace --update "/hudson/authorizationStrategy/@class" --value "hudson.security.AuthorizationStrategy\$Unsecured" ~/.jenkins/config.xml
 xml edit --inplace --delete "/hudson/securityRealm/*" ~/.jenkins/config.xml
 xml edit --inplace --update "/hudson/securityRealm/@class" --value "hudson.security.SecurityRealm\$None" ~/.jenkins/config.xml
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.jenkins-lts.plist
+
+if [ "${OS}" = "Linux" ]; then
+    sudo service jenkins start
+elif [ "${OS}" = "Darwin" ]; then
+    launchctl load ~/Library/LaunchAgents/homebrew.mxcl.jenkins-lts.plist
+fi
