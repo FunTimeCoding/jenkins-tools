f7520940dae843c3f85023f89e289eacbf8ff271
Martin Weber <martin.weber@adition.com>
2015-09-02 11:34:43 +0200

    fixed logic error

--- bin/list-plugins.sh
+++ bin/list-plugins.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(cd "$(dirname "${0}")"; pwd)
 validate_cli
 JENKINS_PLUGINS_CACHE="/tmp/jenkins-plugins-cache.txt"
 
-if [ ! -f ${JENKINS_PLUGINS_CACHE} ] || [ "$(find ${JENKINS_PLUGINS_CACHE} -mmin +2)" = "" ]; then
+if [ ! -f ${JENKINS_PLUGINS_CACHE} ] || [ ! "$(find ${JENKINS_PLUGINS_CACHE} -mmin +2)" = "" ]; then
     ${JENKINS_CMD} list-plugins > "${JENKINS_PLUGINS_CACHE}"
 fi
 
