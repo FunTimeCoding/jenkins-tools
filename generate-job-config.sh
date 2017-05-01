46c5606ddfeb1d2180a71e7dc7085c59b07ca053
Alexander Reitzel <funtimecoding@gmail.com>
2017-05-01 23:54:20 +0200

    remove path variable from job config

--- generate-job-config.sh
+++ generate-job-config.sh
@@ -1,5 +1,4 @@
 #!/bin/sh -e
 
 # shellcheck disable=SC2016
-jjm --locator https://github.com/FunTimeCoding/jenkins-tools.git --build-command 'export PATH="${HOME}/.cabal/bin:${HOME}/.local/bin:/usr/local/bin:${PATH}"
-./build.sh' > job.xml
+jjm --locator https://github.com/FunTimeCoding/jenkins-tools.git --build-command ./build.sh > job.xml
