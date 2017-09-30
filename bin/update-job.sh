621fb2511ccd7a27162c332c462edbec0612cdcb
Alexander Reitzel <funtimecoding@gmail.com>
2017-09-30 20:24:37 +0200

    add script to update an existing job

---
+++ bin/update-job.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -e
+
+DIRECTORY=$(dirname "${0}")
+SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
+
+usage()
+{
+    echo "Local usage: ${0} JOB_NAME JOB_CONFIG"
+}
+
+# shellcheck source=/dev/null
+. "${SCRIPT_DIRECTORY}/../lib/jenkins.sh"
+JOB_NAME="${1}"
+JOB_CONFIG="${2}"
+
+if [ "${JOB_NAME}" = "" ] || [ "${JOB_CONFIG}" = "" ]; then
+    usage
+
+    exit 1
+fi
+
+JOB_CONFIG=$(${REALPATH} "${JOB_CONFIG}")
+
+if [ ! -f "${JOB_CONFIG}" ]; then
+    echo "Config not found: ${JOB_CONFIG}"
+
+    exit 1
+fi
+
+${JENKINS} update-job "${JOB_NAME}" < "${JOB_CONFIG}"
