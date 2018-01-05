a2b3ba0a214af20aac92c093f57d716809369ab9
Alexander Reitzel <funtimecoding@gmail.com>
2018-01-05 16:38:15 +0100

    add script to run script

---
+++ bin/run-script.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+
+DIRECTORY=$(dirname "${0}")
+SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
+
+usage()
+{
+	echo "Local usage: ${0} SCRIPT_FILE"
+}
+
+# shellcheck source=/dev/null
+. "${SCRIPT_DIRECTORY}/../lib/jenkins.sh"
+SCRIPT_FILE="${1}"
+
+if [ "${SCRIPT_FILE}" = "" ]; then
+	usage
+
+	exit 1;
+fi
+
+CONTENTS=$(cat ${SCRIPT_FILE})
+curl --insecure --user "${USERNAME}:${PASSWORD}" --data-urlencode "script=${CONTENTS}" "https://${HOST_NAME}/scriptText"
