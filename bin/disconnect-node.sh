96e2be401737c72e829bb7bd05a745c6fc0282f6
Alexander Reitzel <funtimecoding@gmail.com>
2019-02-12 16:33:22 +0100

    add wrapper scripts for taking nodes offline and online as well as connecting and disconnecting

---
+++ bin/disconnect-node.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+
+DIRECTORY=$(dirname "${0}")
+SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
+
+usage()
+{
+    echo "Local usage: ${0} NODE_NAME"
+}
+
+# shellcheck source=/dev/null
+. "${SCRIPT_DIRECTORY}/../lib/jenkins_tools.sh"
+NODE_NAME="${1}"
+OUTPUT_FILE="${2}"
+
+if [ "${NODE_NAME}" = "" ]; then
+    usage
+
+    exit 1
+fi
+
+${JENKINS} disconnect-node "${NODE_NAME}"
+echo
